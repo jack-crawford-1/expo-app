cee7ad849f23279e470d1cc90f3c5ba725faa37f
Jack Crawford <primes_unit.05@icloud.com>
2025-04-24 07:00:32 +1200

    vercel analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { inject } from '@vercel/analytics';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -20,6 +24,10 @@ export default function RootLayout() {
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
+
+      if (typeof window !== 'undefined') {
+        inject();
+      }
     }
   }, [loaded]);
 
